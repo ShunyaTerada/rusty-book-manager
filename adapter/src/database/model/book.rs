4a7f0864fb619b6f7913c75f64290ab55e51a8a3
ShunyaTerada <shunyaterada@gmail.com>
2026-05-17 10:35:41 +0900

    feat(adapter):BookRow構造体からBook構造体へ変換するFromトレイトの実装

--- adapter/src/database/model/book.rs
+++ adapter/src/database/model/book.rs
@@ -5,5 +5,25 @@ pub struct BookRow {
     pub book_id: Uuid,
     pub title: String,
     pub author: String,
+    pub isbn: String,
     pub description: String,
+}
+
+impl From<BookRow>  for Book {
+    fn from(value: BookRow) -> Self {
+        let BookRow {
+            book_id,
+            title,
+            author,
+            isbn,
+            description,
+        } = value;
+        Self {
+            id: book_id,
+            title,
+            author,
+            isbn,
+            description,
+        }
+    }
 }
