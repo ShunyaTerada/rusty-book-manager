f6eca3ee4241de986b72d71dfa6ffd7033897437
ShunyaTerada <shunyaterada@gmail.com>
2026-05-17 12:07:47 +0900

    test(adapter):蔵書の作成、読み取りクエリのテストコードを実装

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -1,11 +1,12 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use derive_new::new;
-use kernel::model::book::{event::CreateBook, Book};
+use kernel::model::book::{Book, event::CreateBook};
 use kernel::repository::book::BookRepositry;
 use uuid::Uuid;
 
 use crate::database::ConnectionPool;
+use crate::database::model::book::BookRow;
 
 #[derive(new)]
 pub struct BookRepositryImpl {
@@ -33,11 +34,11 @@ impl BookRepositry for BookRepositryImpl {
 
     async fn find_all(&self) -> Result<Vec<Book>> {
         let rows: Vec<BookRow> = sqlx::query_as!(
-           BoodRow,
+            BookRow,
             r#"
                 SELECT book_id, title, author, isbn, description
                 FROM books
-                ORDERD BY create_at DESC
+                ORDER BY created_at DESC
             "#
         )
         .fetch_all(self.db.inner_ref())
@@ -48,22 +49,71 @@ impl BookRepositry for BookRepositryImpl {
 
     async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
         let rows: Option<BookRow> = sqlx::query_as!(
-        BookRow,
-        r#"
+            BookRow,
+            r#"
             SELECT
                 book_id,
                 title,
                 author,
                 isbn,
-                description,
+                description
             FROM books
             WHERE book_id = $1
         "#,
-        book_id
+            book_id
         )
         .fetch_optional(self.db.inner_ref())
         .await?;
 
         Ok(rows.map(Book::from))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[sqlx::test]
+    async fn test_register_book(pool: sqlx::PgPool) -> anyhow::Result<()> {
+        //BookRepositryImplを初期化
+        let repo = BookRepositryImpl::new(ConnectionPool::new(pool));
+
+        //投入するための蔵書データを作成
+        let book = CreateBook {
+            title: "Test Title".into(),
+            author: "Test Author".into(),
+            isbn: "Test ISBN".into(),
+            description: "Test Description".into(),
+        };
+
+        //蔵書データを投入すると正常終了することを確認
+        repo.create(book).await?;
+
+        //蔵書の一覧を取得すると投入した一件だけ取得することを確認
+        let res = repo.find_all().await?;
+        assert_eq!(res.len(), 1);
+
+        //蔵書の一覧の最初のデータから蔵書IDを取得し、
+        //find_by_idメソッドでその蔵書データを取得できることを確認
+        let book_id = res[0].id;
+        let res = repo.find_by_id(book_id).await?;
+        assert!(res.is_some());
+
+        //取得した蔵書データがCreateBookで投入した
+        //蔵書データと一致することを確認
+        let Book {
+            id,
+            title,
+            author,
+            isbn,
+            description,
+        } = res.unwrap();
+        assert_eq!(id, book_id);
+        assert_eq!(title, "Test Title");
+        assert_eq!(author, "Test Author");
+        assert_eq!(isbn, "Test ISBN");
+        assert_eq!(description, "Test Description");
+
+        Ok(())
     }
 }
