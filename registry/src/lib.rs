4b21bae9debb11eab6f2c5d09ca414d3775c907e
ShunyaTerada <shunyaterada@gmail.com>
2026-05-18 08:06:50 +0900

    feat(registry):書籍用のDIコンテナを実装

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,25 +1,33 @@
 use std::sync::Arc;
 
-use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
-use kernel::repository::health::HealthCheckRepository;
+use adapter::{database::ConnectionPool, repository::{book::BookRepositryImpl,health::HealthCheckRepositoryImpl}};
+use kernel::{model::book::Book, repository::{book::BookRepositry,health::HealthCheckRepository}};
+
 
 //DIコンテナの役割を果たす構造体定義する。Cloneは後ほどaxum側で必要になるため。
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check_repository: Arc<dyn HealthCheckRepository>,
+    book_repository: Arc<dyn BookRepositry>,
 }
 
 impl AppRegistry {
     pub fn new(pool: ConnectionPool) -> Self {
         //依存解決を行う。関数内で手書きする。
         let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
+        let book_repository = Arc::new(BookRepositryImpl::new(pool.clone()));
         Self {
             health_check_repository,
+            book_repository
         }
     }
 
     //依存解決したインスタンスを返すメソッドを定義する。
     pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
         self.health_check_repository.clone()
+    }
+
+    pub fn book_repository(&self) -> Arc<dyn BookRepositry> {
+        self.book_repository.clone()
     }
 }
