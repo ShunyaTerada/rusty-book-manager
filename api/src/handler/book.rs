43c602477fad3ef5295c03138e72f4fad57165b9
ShunyaTerada <shunyaterada@gmail.com>
2026-05-19 08:00:05 +0900

    feat(api/handler):書籍の暫定的なエラーハンドリングを追加

--- api/src/handler/book.rs
+++ api/src/handler/book.rs
@@ -9,3 +9,14 @@ use thiserror::Error;
 use uuid::Uuid;
 
 use crate::model::book::{BookResponse, CreateBookRequest};
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("{ 0 }")]
+    InternalError(#[from] anyhow::Error),
+}
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "").into_response()
+    }
+}
