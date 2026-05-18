64502bb30815330b05d322bba34322f4ab3d1539
ShunyaTerada <shunyaterada@gmail.com>
2026-05-19 08:56:33 +0900

    fix(api/route):旧バージョン仕様だったルーティングコードを修正

--- api/src/route/health.rs
+++ api/src/route/health.rs
@@ -8,6 +8,6 @@ pub fn build_health_check_routers() -> Router<AppRegistry> {
     //ヘルスチェックに関連するパスのルートである'/health'に個別のパスをネストする
     let routers = Router::new()
         .route("/", get(health_check))
-        .route("/", get(health_check_db));
+        .route("/db", get(health_check_db));
     Router::new().nest("/health", routers)
 }
