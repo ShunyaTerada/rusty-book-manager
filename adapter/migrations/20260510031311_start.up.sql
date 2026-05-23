0a34ce86f30fef75f51521c3195c4691cce29d13
ShunyaTerada <shunyaterada@gmail.com>
2026-05-23 12:50:13 +0900

    build(migration):ユーザー及び権限テーブルの追加

--- adapter/migrations/20260510031311_start.up.sql
+++ adapter/migrations/20260510031311_start.up.sql
@@ -6,6 +6,32 @@ CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS '
     END;
 '   LANGUAGE 'plpgsql';
 
+-- rolesテーブルの作成
+CREATE TABLE IF NOT EXISTS roles (
+    role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL UNIQUE
+)
+
+-- usersテーブルの作成
+CREATE TABLE IF NOT EXISTS users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role_id UUID NOT NULL,
+    created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+-- users テーブルのupdated_atを自動更新するためのトリガー
+CREATE TRIGGER users_updated_at 
+    BEFORE UPDATE ON users FOR EACH ROW
+    EXECUTE PROCEDURE set_updated_at();
+
 -- books テーブルの作成
 CREATE TABLE IF NOT EXISTS books(
     book_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -13,8 +39,13 @@ CREATE TABLE IF NOT EXISTS books(
     author VARCHAR(255) NOT NULL,
     isbn VARCHAR(255) NOT NULL,
     description VARCHAR(1024) NOT NULL,
+    user_id UUID NOT NULL,
     created_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    updated_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
+    updated_at TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
 -- books テーブルへのトリガーの追加
