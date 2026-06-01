1e40f395e657c1a3c3a02f2341f1610c8fd9039f
ShunyaTerada <shunyaterada@gmail.com>
2026-06-02 07:23:14 +0900

    fix(UserRepository):createメソッドの戻り値の型を修正

--- kernel/src/repository/user.rs
+++ kernel/src/repository/user.rs
@@ -13,7 +13,7 @@ use crate::model::{
 pub trait UserRepository: Send + Sync {
     async fn find_current_user(&self, current_user_id: UserId) -> AppResult<Option<User>>;
     async fn find_all(&self) -> AppResult<Vec<User>>;
-    async fn create(&self, event: CreateUser) -> AppResult<()>;
+    async fn create(&self, event: CreateUser) -> AppResult<User>;
     async fn update_password(&self, event: UpdateUserPassword) -> AppResult<()>;
     async fn update_role(&self, event: UpdateUserRole) -> AppResult<()>;
     async fn delete(&self, event: DeleteUser) -> AppResult<()>;
