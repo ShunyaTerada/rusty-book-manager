bf7834e0fd924a798571fa88e6b8af4d4c990584
ShunyaTerada <shunyaterada@gmail.com>
2026-06-03 08:29:19 +0900

    feat(handler(user)):get_current_userハンドラーの仮実装

--- api/src/handler/user.rs
+++ api/src/handler/user.rs
@@ -1,46 +1,59 @@
-use registry::AppRegistry
 use axum::{
-    http::{
-        status,
-        StatusCode,
-    },
     Json,
-    RequestPartsExt,
-    
+    extract::{Path, Query, State},
+    http::StatusCode,
 };
-use axum_extra::{
-    
-};
+use registry::AppRegistry;
 
 use kernel::model::{
-    role::Role,
     id::UserId,
-    user::{
-        User,
-        event::{
-            CreateUser,
-            UpdateUserRole,
-            UpdateUserPassword,
-            DeleteUser,
-        }
-    },
+    role::Role,
+    user::event::{CreateUser, DeleteUser, UpdateUserPassword, UpdateUserRole},
 };
+use utoipa::Path;
 
 use crate::{
     extractor::AuthorizedUser,
     model::user::{
-        RoleName,
-        UserResponse,
-        UsersResponse,
-        CreateUserRequest,
-        UpdateUserRoleRequest,
-        UpdateUserRoleRequestWithUserId,
-        UpdateUserPasswordRequest,
-        UpdateUserPasswordRequestWithUserId,
-    }
+        CreateUserRequest, RoleName, UpdateUserPasswordRequest,
+        UpdateUserPasswordRequestWithUserId, UpdateUserRoleRequest,
+        UpdateUserRoleRequestWithUserId, UserResponse, UsersResponse,
+    },
 };
 
-use shared::error::{AppResult, AppError};
+use shared::error::{AppError, AppResult};
 
+pub async fn get_current_user(
+    State(registry): State<AppRegistry>,
+    Path(user_id): Path<UserId>,
+) -> AppResult<Json<UserResponse>> {
+    registry
+        .user_repository()
+        .find_current_user(user_id)
+        .await?
+        .map(|e| Json(UserResponse::from(e)))
+        .and_then(|opt| match opt {
+            Some(e) => Ok(e),
+            None => AppError::EntityNotFound("アカウントが見つかりません".to_string()),
+        })
+}
 
+pub async fn list_user() {
+    todo!()
+}
 
+pub async fn register_user() {
+    todo!()
+}
+
+pub async fn change_role() {
+    todo!()
+}
+
+pub async fn change_password() {
+    todo!()
+}
+
+pub async fn delete_user() {
+    todo!()
+}
