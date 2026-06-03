1c18cb4958350ba4755ed643ea5f0409bd9eb6f7
ShunyaTerada <shunyaterada@gmail.com>
2026-06-04 06:31:14 +0900

    fix(handler(user)):get-current_userメソッドを修正

--- api/src/handler/user.rs
+++ api/src/handler/user.rs
@@ -10,7 +10,6 @@ use kernel::model::{
     role::Role,
     user::event::{CreateUser, DeleteUser, UpdateUserPassword, UpdateUserRole},
 };
-use utoipa::Path;
 
 use crate::{
     extractor::AuthorizedUser,
@@ -23,19 +22,8 @@ use crate::{
 
 use shared::error::{AppError, AppResult};
 
-pub async fn get_current_user(
-    State(registry): State<AppRegistry>,
-    Path(user_id): Path<UserId>,
-) -> AppResult<Json<UserResponse>> {
-    registry
-        .user_repository()
-        .find_current_user(user_id)
-        .await?
-        .map(|e| Json(UserResponse::from(e)))
-        .and_then(|opt| match opt {
-            Some(e) => Ok(e),
-            None => AppError::EntityNotFound("アカウントが見つかりません".to_string()),
-        })
+pub async fn get_current_user(user: AuthorizedUser) -> AppResult<Json<UserResponse>> {
+    Ok(Json(UserResponse::from(user.user)))
 }
 
 pub async fn list_user() {
