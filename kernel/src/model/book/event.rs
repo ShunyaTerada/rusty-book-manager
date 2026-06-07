c27792e33f602918f8c7f57bc07a76d6790caf97
ShunyaTerada <shunyaterada@gmail.com>
2026-06-07 09:10:15 +0900

    feat(book):書籍の更新・削除用の型を追加

--- kernel/src/model/book/event.rs
+++ kernel/src/model/book/event.rs
@@ -1,6 +1,22 @@
+use crate::model::id::{BookId, UserId};
+
 pub struct CreateBook {
     pub title: String,
     pub author: String,
     pub isbn: String,
     pub description: String,
+}
+
+pub struct UpdateBook {
+    pub book_id: BookId,
+    pub title: String,
+    pub author: String,
+    pub isbn: String,
+    pub description: String,
+    pub requested_user: UserId,
+}
+
+pub struct DeleteBook {
+    pub book_id: BookId,
+    pub requested_user: UserId,
 }
