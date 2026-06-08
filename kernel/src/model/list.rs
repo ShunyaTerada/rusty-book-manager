e2c03669bce7261ce071df508e263dcb0750c493
ShunyaTerada <shunyaterada@gmail.com>
2026-06-09 07:03:48 +0900

    chore:微修正

--- kernel/src/model/list.rs
+++ kernel/src/model/list.rs
@@ -3,11 +3,11 @@ pub struct PaginatedList<T> {
     pub total: i64,
     pub limit: i64,
     pub offset: i64,
-    pub item: Vec<T>,
+    pub items: Vec<T>,
 }
 
 impl<T> PaginatedList<T> {
     pub fn into_inner(self) -> Vec<T> {
-        self.item
+        self.items
     }
 }
