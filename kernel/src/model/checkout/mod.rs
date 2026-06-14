1a99e15f25db8fae964c5f7acb9d8193e0afbed7
ShunyaTerada <shunyaterada@gmail.com>
2026-06-14 11:43:18 +0900

    feat(kernel):貸出の型を定義

--- kernel/src/model/checkout/mod.rs
+++ kernel/src/model/checkout/mod.rs
@@ -1,1 +1,20 @@
+use chrono::{DateTime, Utc};
+
+use crate::model::id::{BookId, CheckoutId, UserId};
+
 pub mod event;
+
+pub struct Checkout {
+    pub id: CheckoutId,
+    pub checked_out_by: DateTime<Utc>,
+    pub checked_out_at: UserId,
+    pub returned_at: Option<DateTime<Utc>>,
+    pub book: CheckoutBook,
+}
+
+pub struct CheckoutBook {
+    pub book_id: BookId,
+    pub title: String,
+    pub author: String,
+    pub isbn: String,
+}
